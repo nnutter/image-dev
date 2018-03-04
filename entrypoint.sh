8ce01a7e0814265fed8a5391439c83b33f84d651
Nathaniel Nutter <iam@nnutter.com>
2018-03-04 14:04:54 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,14 @@
 
 set -o errexit -o nounset -o pipefail
 
-git --git-dir ~/.dotfiles.git --work-tree ~ init
-git --git-dir ~/.dotfiles.git               config status.showUntrackedFiles no
-git --git-dir ~/.dotfiles.git               remote add -f origin https://github.com/nnutter/dotfiles.git
-git --git-dir ~/.dotfiles.git --work-tree ~ checkout -f master
-git --git-dir ~/.dotfiles.git               remote set-url origin git@github.com:nnutter/dotfiles.git
+if ! test -d ~/.dotfiles.git
+then
+  git --git-dir ~/.dotfiles.git --work-tree ~ init
+  git --git-dir ~/.dotfiles.git               config status.showUntrackedFiles no
+  git --git-dir ~/.dotfiles.git               remote add -f origin https://github.com/nnutter/dotfiles.git
+  git --git-dir ~/.dotfiles.git --work-tree ~ checkout -f master
+  git --git-dir ~/.dotfiles.git               remote set-url origin git@github.com:nnutter/dotfiles.git
+fi
 
 git config --file ~/.config/git/config transfer.fsckObjects false
 git --git-dir ~/.dotfiles.git --work-tree ~ submodule update --init
